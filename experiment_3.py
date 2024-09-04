8e1993465af41305cd9d5df7332b47aa4b7d9341
prashantrai-30 <prashantkumar2488@gmail.com>
2024-09-04 11:12:06 +0530

    Added python lab work

---
+++ experiment_3.py
@@ -0,0 +1,8 @@
+'''
+WAP to temperature in celcius to fahrenheit conversion Fahrenheit = celcius * 1.8 +32 
+
+'''
+
+temperature = int(input("Enter temperature in Celcius:"))
+FaherenheitTempreture = temperature* 1.8 + 32
+print("Temperature in Fahrenheit:",FaherenheitTempreture)
